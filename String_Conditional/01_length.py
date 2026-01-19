8599e6d3a350c8ba625791931d61512c2e08c076
divyanka206 <divyankakanyal@gmail.com>
2026-01-19 08:05:00 +0530

    commit

---
+++ String_Conditional/01_length.py
@@ -0,0 +1,13 @@
+# len() function shows the total no if characters in a string
+# space & special characters {!,#,@,%,$} are counted in length of str
+
+# str1 = "Divyanka #yo"  #total characters 12 (including space and #)
+# print(len(str1))
+
+# Concatenation: Addition of two strings
+
+str2 = "Good"+" "+"Morning"+"hello%1" #total characters = 19
+print(len(str2)) #19
+print(str2)   #Good Morninghello%1
+
+
